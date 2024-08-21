c3ab769e44b072e6d1fa3e6bfcde1f88bdc268e2
giveon <93718135+sohsag@users.noreply.github.com>
2024-08-22 00:35:15 +0200

    Update buy.py

--- buy.py
+++ buy.py
@@ -1,41 +1,45 @@
 from web3 import Web3
 
-w3 = Web3(Web3.HTTPProvider('https://1rpc.io/bnb'))
+
+def buy(contract_address_of_coin, amount, public_key, private_key):
+    w3 = Web3(Web3.HTTPProvider('https://1rpc.io/bnb'))
+
+    contract_address = '0xEC4549caDcE5DA21Df6E6422d448034B5233bFbC'
+    function_signature = '0x3deec419'
+
+    # contract address, change 1 to whatever. e.g. 0.1 for 0.1 BNB etc., leave as is
+    input_params = [contract_address_of_coin, 1000000000000000000 * amount, 1000000]
+
+    calldata = function_signature + '000000000000000000000000'
+    for param in input_params:
+        if isinstance(param, str) and param.startswith('0x'):
+            calldata += param[2:]
+        else:
+            calldata += f'{int(param):064x}'
 
 
-contract_address = '0xEC4549caDcE5DA21Df6E6422d448034B5233bFbC'
-function_signature = '0x3deec419'
+    nonce = w3.eth.get_transaction_count(public_key)
 
-# contract address of coin, change 1 to whatever. e.g. 0.1 for 0.1 BNB etc., leave as is
-input_params = ['0x5c9e15ff4aded6b1ebcbc83f18f6554175bd82ac', 1000000000000000000 * 1, 1000000]
+    tx = {
+        'to': contract_address,
+        'data': calldata,
+        'gas': 140000,
+        'gasPrice': w3.to_wei('3', 'gwei'),
+        'nonce': nonce,
+        'chainId': 56,  # Binance Smart Chain mainnet chainId
+        'value': 1100000000000000000 * amount  # change 1 to whatever. e.g. 0.1 for 0.1 BNB etc.
+    }
 
+     # privkey with 0x infront
+    signed_tx = w3.eth.account.sign_transaction(tx, private_key=private_key)
 
-calldata = function_signature + '000000000000000000000000'
-for param in input_params:
-    if isinstance(param, str) and param.startswith('0x'):
-        calldata += param[2:]
-    else:
-        calldata += f'{int(param):064x}'
-
-
-sender_address = '' # ur address
-nonce = w3.eth.get_transaction_count(sender_address)
-
-
-tx = {
-    'to': contract_address,
-    'data': calldata,
-    'gas': 140000,
-    'gasPrice': w3.to_wei('3', 'gwei'),
-    'nonce': nonce,
-    'chainId': 56,  # Binance Smart Chain mainnet chainId
-    'value': 1100000000000000000 * 1 # change 1 to whatever. e.g. 0.1 for 0.1 BNB etc.
-}
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+    print(f'https://bscscan.com/tx/0x{tx_hash.hex()}')
 
 
-private_key = '' # privkey with 0x infront
-signed_tx = w3.eth.account.sign_transaction(tx, private_key=private_key)
-
-
-tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
-print(f'https://bscscan.com/tx/0x{tx_hash.hex()}')
+if __name__ == '__main__':
+    ca = input('contract address of coin ')
+    amount = float(input('amount of BNB '))
+    public_key = input('public key of ur wallet ')
+    private_key = input('private key of ur wallet ')
+    buy(ca, amount, public_key, private_key)
